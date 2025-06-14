29fcacd45d51d2c0e26191b6507a86c60780ab76
Ramona Flores <ramonaflores@Ramonas-MacBook-Pro.local>
2025-06-14 00:24:26 -0500

    feat: part of the data ingestion dag

--- dags/ingest_customer_data_dag.py
+++ dags/ingest_customer_data_dag.py
@@ -11,7 +11,25 @@ from dotenv import load_dotenv
 #Let's load our .env variables ! inside the docker container
 
 load_dotenv("/opt/airflow/.env")
+# URL where the JSON is located at Amazon Simple Storage Service (or ...S3 haha)
 
 S3_URL=os.getenv("S3_URL")
+#the downloaded data file path, where it will be stored inside the container
 LOCAL_PATH=os.getenv("LOCAL_PATH")
+#postgresql connection string
 DB_CONN_STR=os.getenv("DB_CONN_STR")
+
+
+
+def download_json():
+    #performs  a GET REQUEST at S3
+    response=requests.get(S3_URL)
+    #Throws an error if the request fails
+    response.raise_for_status()
+
+    #saves the content of the JSON file
+    with open(LOCAL_PATH, "w") as f:
+        f.write(response.text)
+    
+    print(f"File saved at:{LOCAL_PATH}")
+
