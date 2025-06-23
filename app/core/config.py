ee44bdb90b3075bd91f826750b0d890cc84dc34b
Juan José Angarita Yela <angaritayelaj@gmail.com>
2025-06-23 13:30:10 -0500

    Extra feature:  Moved the Backend  Directory to another repo

--- app/core/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    DATABASE_URL: str = (
-        "postgresql+psycopg2://qversity-admin:qversity-admin@localhost:5432/qversity"
-    )
-
-settings = Settings()
--- app/db/repositories.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-
-def get_all(db: Session, model):
-    return db.query(model).all()
-
-def to_dict(obj):
-    data = obj.__dict__.copy()
-    data.pop("_sa_instance_state", None)
-    return data
